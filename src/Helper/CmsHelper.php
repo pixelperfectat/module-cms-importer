21a8d6a08e84aef8e7f122e871d05225d67e8371
Andre Flitsch <pixelhed@pixelperfect.at>
2026-07-02 08:38:04 +0200

    Fix phpcbf auto-fixes, exclude sync-conflict files from phpcs, remove rector conflict

--- src/Helper/CmsHelper.php
+++ src/Helper/CmsHelper.php
@@ -21,11 +21,6 @@ use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Module\Dir\Reader as DirectoryReader;
 
-/**
- * Class CmsHelper
- *
- * @package PixelPerfect\CmsImporter\Helper
- */
 class CmsHelper
 {
 
